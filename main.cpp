7c280e49cffd1420510d3fd311a96359f880b0a9
Umair Jibran <umairjibran7@gmail.com>
2020-06-21 12:57:17 +0000

    completed the program

--- main.cpp
+++ main.cpp
@@ -20,9 +20,10 @@ class Pizza{
       }
     }
     void addToppings(int num){
-      int temp;
+      int temp = 0;
       while(num > temp){
         totalCost = totalCost + 2;
+        temp = temp + 1;
       }
     }
     double calcCost(){
@@ -33,7 +34,18 @@ class Pizza{
 int main() {
   Pizza p;
   int size;
+  string tops[3] = {"Cheese","Pepperoni","Ham"};
   cout << "set size, 1 for small, 2 for medium, 3 for large: ";
   cin >> size;
-  
+  p.setSize(size);
+  int i = 0;
+  while(i < 3){
+    int num = 0;
+    cout << "How much topping would you want for " << tops[i];
+    cin >> num;
+    p.addToppings(num);
+    i = i + 1;
+  }
+  double price = p.calcCost();
+  cout << endl << "total cost is $" << price;
 }
